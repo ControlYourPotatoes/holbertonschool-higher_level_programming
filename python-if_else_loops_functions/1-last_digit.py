1979d760a4a5de028b8c236cae172a12551e9212
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-09-29 14:16:50 -0400

    fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,3 @@
-#This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
