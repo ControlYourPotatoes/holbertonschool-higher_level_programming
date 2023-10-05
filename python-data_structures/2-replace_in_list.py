c38eb56c7ddeff60d9cbf64e43c391941c558d31
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-05 12:54:01 -0400

    spacing

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return(my_list)
@@ -7,4 +8,5 @@ def replace_in_list(my_list, idx, element):
     else:
         my_list[idx] = element
         return(my_list)
+
     
