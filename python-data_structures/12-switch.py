f67c56b6e4277801e4747264859e00373bf450ee
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-05 13:52:38 -0400

    12 switch

---
+++ python-data_structures/12-switch.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def switch(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
+    if len(my_list) == 1:
+        return(my_list)
+    my_list[0], my_list[-1] = my_list[-1], my_list[0]
+    return(my_list)
