0318dd1735e602925bc2513451352235d9743c63
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-12 16:35:49 -0400

    indent

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,14 +3,14 @@
 
 
 class Square:
-        """Definition of square attribute"""
-        def __init__(self, size=0):
-            self.__size = size
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-            if size < 0:
+    """Definition of square attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
                 raise ValueError("size must be >= 0")
 
-        def area(self):
-            """This method return the area of the square"""
-            return self.__size ** 2
+    def area(self):
+        """This method return the area of the square"""
+        return self.__size ** 2
