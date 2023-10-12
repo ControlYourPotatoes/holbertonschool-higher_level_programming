3bfbe84f2e28056ffe37f1e769db9dbb63d41843
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-12 16:16:31 -0400

    t3

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,5 +5,6 @@
 class Square:
     """Definition of square attribute"""
     def __init__(self, size):
-        self._Square__size = size #an attribute of the class Square
+        self._Square__size = size  #an attribute of the class Square
+
 
