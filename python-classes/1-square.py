adc12e4220fbe35ef2f5ae7cc60b05623bd882d7
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-12 16:07:30 -0400

    msg

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""This module have a class that defines a square """
+# A class Square that defines a squareclass Square:
 class Square:
-    """Definition of square attribute"""
-    def __init__(self, size):
-        """Definition of square attribute"""
-        self._Square__size = size
+    def __init__(self, size): #Definition of square attribute
+        self._Square__size = size #an attribute of the class Square
 
