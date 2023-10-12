6d309c8164bc8a6be80d3a88263197f8878272f3
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-12 19:35:39 -0400

    6square

---
+++ python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""This module have a class that defines a square """
+
+
+class Square:
+    """Definition of square attribute"""
+    def __init__(self, size=0):
+        """Initialize square"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """check size"""
+        return (self.__size)
+    
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) is not 2:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    @size.setter
+    def size(self, value):
+        """square size"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self._Square__size = value
+
+    def area(self):
+        """return square area"""
+        return self._Square__size ** 2
+
+
+    def my_print(self):
+        """print square"""
+        if self.__size is not 0:
+            for i in range(self.__size):
+                print("{}".format("#" * self.__size))
+        else:
+            print()
