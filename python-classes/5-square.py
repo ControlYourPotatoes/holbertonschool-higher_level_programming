81f280856ce7d22e187caedd2792e54ad610b7f1
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-12 19:32:05 -0400

    5square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -13,12 +13,12 @@ class Square:
 
     @property
     def size(self):
-        """return the sqr size"""
+        """check size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """set the sqr size"""
+        """square size"""
         if type(value) is not int:
             raise TypeError('size must be an integer')
         elif value < 0:
@@ -27,11 +27,12 @@ class Square:
             self._Square__size = value
 
     def area(self):
-        """return sqr area"""
+        """return square area"""
         return self._Square__size ** 2
 
 
     def my_print(self):
+        """print square"""
         if self.__size is not 0:
             for i in range(self.__size):
                 print("{}".format("#" * self.__size))
