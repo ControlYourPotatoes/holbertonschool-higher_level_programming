ed5a8a347c32964cace05df5a1b6b26ef32c6e98
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-13 19:49:16 -0400

    print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This function will print My name is <first name> <last name>"""
+
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            print("#" * size)
+
+print_square(4)
