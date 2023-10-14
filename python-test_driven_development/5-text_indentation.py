c1c8fdae20f664f69c119005a8ff6ba2c5a657a0
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-13 20:19:12 -0400

    flag

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,14 +5,21 @@ def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
     """
+    flag = True
+    delimiters = ['?', '.', ':']
+    
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for let in range(len(text)):
-        if text[let] == '.' or text[let] == '?' or text[let] == ':':
-            print(text[let])
-            print()
-        elif text[let] == " " and text[let - 1] in ['.', '?', ':']:
+
+    for ch in text:
+        if ch == ' ' and flag is True:
             continue
+
+        if ch in delimiters:
+            print("{}".format(ch), end="")
+            print("\n")
+            flag = True
         else:
-            print(text[let], end="")
+            print(ch, end="")
+            flag = False
