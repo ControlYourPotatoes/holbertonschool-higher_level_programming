71ea0f4cde716f3619ca5b6a32e6e5ac36d4a09d
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-13 20:16:04 -0400

    fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,13 +8,11 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new_text = ""
-    for char in text:
-        new_text += char
-        if char in [".", "?", ":"]:
-            new_text += "\n\n"
-
-    lines = [line.strip() for line in new_text.split("\n")]
-    for line in lines:
-        print(line)
-
+    for let in range(len(text)):
+        if text[let] == '.' or text[let] == '?' or text[let] == ':':
+            print(text[let])
+            print()
+        elif text[let] == " " and text[let - 1] in ['.', '?', ':']:
+            continue
+        else:
+            print(text[let], end="")
