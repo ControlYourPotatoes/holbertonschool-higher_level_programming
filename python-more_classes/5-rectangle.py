5aa77527b6fddd7c162ad6a26637f13ea46d5e61
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-13 22:42:38 -0400

    345

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,7 @@
+
+    def __repr__(self):
+        """return a string representation of
+        the rectangle to be able to recreate
+        a new instance by"""
+
+        return "Rectangle({}, {})".format(self.width, self.height)
