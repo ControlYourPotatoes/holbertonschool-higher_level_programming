fffe58d048c4dff743ac3a3fdd96a34ff99400d9
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-13 20:20:56 -0400

    space

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """This module print texts"""
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
     """
     flag = True
     delimiters = ['?', '.', ':']
-    
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
 
     for ch in text:
         if ch == ' ' and flag is True:
