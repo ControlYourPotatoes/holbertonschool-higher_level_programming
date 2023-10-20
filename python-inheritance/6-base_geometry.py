bbca7905c6c4e63d48018e8129c3406a98fa8af5
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 18:23:16 -0400

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry Module"""
+
+
+class BaseGeometry:
+    """Define BaseGeometry class"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
