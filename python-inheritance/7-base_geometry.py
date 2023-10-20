44a2c6b756ad16369f91e51b71e866f96b35b414
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 18:26:44 -0400

    7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Geometry Module"""
+
+
+class BaseGeometry:
+    """Define BaseGeometry class"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
