79a23670eaab3f6e4b4650585ebd585956716610
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 20:14:50 -0400

    8

---
+++ python-inheritance/tests/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """The Rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance."""
+        self.__width = width
+        self.__height = height
+        super().__init__()
