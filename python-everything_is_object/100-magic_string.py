95ac0b13a962fedf646ed7c2e0452ada100078fc
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 23:22:29 -0400

    fix lines

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
+#!/usr/bin/python3
 def magic_string():
-    """Function to create a magic string"""
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return "Holberton" + ", Holberton" * (magic_string.count - 1)
