a0a86aa135830520eb8020e508553b87bb356ba8
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 21:48:52 -0400

    1write

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module has function named write_file"""
+
+
+def write_file(filename="", text=""):
+    """Function that write a
+    string to a text file and
+    return character count
+    """
+    with open(filename, 'w') as fd:
+        return fd.write(text)
