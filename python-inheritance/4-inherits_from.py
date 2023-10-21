ceeaaea0fee4f62fc63d3683dd1e79858722fa95
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 21:20:20 -0400

    spacing

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-This module contains that compares a class
-"""
+"""This module contains that compares a class"""
 
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if obj is an instance of a class that inherited (directly or indirectly) from a_class.
-    """
+    """Returns True if obj is an instance of a class that inherited"""
     if type(obj) is not a_class and issubclass(type(obj), a_class):
         return True
     else:
