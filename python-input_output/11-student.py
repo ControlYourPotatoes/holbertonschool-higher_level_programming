5a5cf71cb84b2676df820f6217476d10be8684fd
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 22:48:59 -0400

    11 reloadjson

---
+++ python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" This module contains class Student """
+
+
+class Student:
+    """
+    A class Student that defines a student by
+    some public instance attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance with the given
+        first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
