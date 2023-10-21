8c83905e7c6a97aecea4a9ca2018e226c3f18ad6
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 23:21:24 -0400

    magic

---
+++ python-everything_is_object/100-magic_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module to create a magic string"""
+
+
+def magic_string():
+    """Function to create a magic string"""
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return "Holberton" + ", Holberton" * (magic_string.count - 1)
