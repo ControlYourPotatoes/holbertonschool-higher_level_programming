bb63edb0b87e215d5154996f382c09aeef1bd22a
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 22:03:30 -0400

    3

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module has function named append_write"""
+"""This module has a function named append_write"""
 
 
 def append_write(filename="", text=""):
---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains a function named
+to_json_string that returns the JSON
+representation of an object
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return json.dumps(my_obj)
