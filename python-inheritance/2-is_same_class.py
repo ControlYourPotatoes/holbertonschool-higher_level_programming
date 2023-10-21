5229b7e1a6e2adf2905990342622508a3a54e159
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 21:17:44 -0400

    spacing

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-This module contains that compares a class
-"""
+"""This module contains that compares a class"""
 
 
 def is_same_class(obj, a_class):
-    """
-    Returns True if the object is exactly an instance of the specified class;
-    otherwise False.
-    """
+    """True if the object is exactly an instance of the specified class"""
     return type(obj) is a_class
-
