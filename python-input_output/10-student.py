0dd9e9b9ed0b5d7252048e57cd910cf4c0d8b427
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 22:46:54 -0400

    trail 12

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -9,7 +9,7 @@ class Student:
     """
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a new Student instance with the given 
+        Initializes a new Student instance with the given
         first_name, last_name and age
         """
         self.first_name = first_name
