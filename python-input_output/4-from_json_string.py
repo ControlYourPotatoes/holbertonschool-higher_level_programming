ed2a1bcc4543390ae29fb3f2e30f1aaea569eef2
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 22:15:32 -0400

    document

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,6 +7,7 @@ import json
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of an object (string)
+    This function returns an objects
+    represented by a JSON string
     """
     return json.loads(my_obj)
