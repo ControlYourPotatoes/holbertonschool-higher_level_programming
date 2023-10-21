6463f9397533d8cfa61e1e41b6789d9fcf654018
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 21:28:59 -0400

    tests 3

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,8 +6,8 @@ class MyList(list):
     """Class MyList that inherits from list"""
     def print_sorted(self):
         """Prints the list, but sorted (ascending sort)"""
-        list_sorted = []
-        for num in self:
-            list_sorted.append(num)
-        list_sorted.sort()
-        print(list_sorted)
+        sorted = []
+        for i in self:
+            sorted.append(i)
+        sorted.sort()
+        print(sorted)
