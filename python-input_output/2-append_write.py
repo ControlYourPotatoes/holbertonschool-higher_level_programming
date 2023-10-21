a4afc02e3218647184e96d3dd9ae779dada74873
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 21:51:19 -0400

    fix

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """This module have a function named append_write"""
 
 
-def write_file(filename="", text=""):
+def append_file(filename="", text=""):
     """ Append string to end of file and return character count"""
-    with open(filename, 'w') as fd:
+    with open(filename, 'a') as fd:
         return fd.write(text)
