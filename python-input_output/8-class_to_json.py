4287b22fcfbe0ad8d2010e55b34df130f7b2806f
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-20 22:35:14 -0400

    8class

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to convert json"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure 
+    """
+    return obj.__dict__
