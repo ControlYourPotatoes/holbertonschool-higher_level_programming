83b4a75a8b1ae912c57b989c7011eafd61df235f
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-27 15:57:47 -0400

    dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,21 +18,25 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
+        """Return the JSON serialization of a list of dicts."""
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file"""
-        filename = cls.__name__ + '.json'
-        with open(filename, 'w') as fd:
-            list_instance = []
+        """Write the JSON serialization of a list of objects to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
             if list_objs is None:
-                fd.write(cls.to_json_string(list_instance))
+                jsonfile.write("[]")
             else:
-                for i in list_objs:
-                    list_instance.append(i.to_dictionary())
-                fd.write(cls.to_json_string(list_instance))
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the deserialization of a JSON string."""
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
