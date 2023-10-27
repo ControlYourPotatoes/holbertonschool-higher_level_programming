4ff42d0f61062bfa12815fb89c8cbaf6c750d927
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-27 14:33:11 -0400

    files and base.py

--- python-input_output/12-main.py
+++ python-input_output/12-main.py
@@ -3,7 +3,7 @@
 12-main
 """
 pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
-
+/home/puga/Documents/Holberton Git/holbertonschool-higher_level_programming/python-almost_a_circle/python-data_structures
 def print_triangle(triangle):
     """
     Print the triangle
