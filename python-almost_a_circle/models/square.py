cc8527eca8f5e1351f95a90dc2cda5edc7fd546e
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-27 15:15:12 -0400

    document str

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,4 +11,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """override __str__ with new string in the format"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
