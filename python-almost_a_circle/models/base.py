6fc3eed7fe36a8ad707e221dbcc8d20138993f3d
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-27 15:43:58 -0400

    json add

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base Class Module"""
 
+import json
+
 
 class Base:
     """Base Class"""
@@ -13,3 +15,23 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of list_dictionaries"""
+        import json
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string representation of list_objs to a file"""
+        import json
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(list_dicts))
