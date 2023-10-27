5443b91d9a26943e77df617100caf501c52ead26
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-27 15:21:37 -0400

    size property

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,14 @@ class Square(Rectangle):
     def __str__(self):
         """override __str__ with new string in the format"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
