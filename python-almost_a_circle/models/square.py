1042fb26969311ce3db1dfaeec08aa03f751df86
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-27 15:11:54 -0400

    document class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Square Class Module"""
 
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square Class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
